9f169ef44f6ab8358fe67b847badc66abdd3d7c5
oshamaa6317 <oshamaa6317@001LAB102010553.neisd.ad>
2015-01-07 13:14:33 -0600

    Spongebob Lives

--- ClassZoo4/src/OceanSponge.java
+++ ClassZoo4/src/OceanSponge.java
@@ -2,7 +2,7 @@ import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
 public class OceanSponge extends Applet implements KeyListener  {
-	Graphics Spongebob;
+	 Graphics Spongebob;
 	public void init(){
 		
 		addKeyListener( this );
@@ -12,24 +12,61 @@ public class OceanSponge extends Applet implements KeyListener  {
 	
 	public void paint(Graphics g){
 		drawHabitat(g);
-		//hi
-		g.setColor(Color.yellow);
-		Spongebob=g.create();
-		Spongebob.fillRect(getWidth()/10, getHeight()/5, 100, 400);
+		drawAnimal(g, 'N');
+
+		
 	}
 	
 	public void keyPressed(KeyEvent arg0) {
 		Character key=arg0.getKeyChar();
-		if(key=='A' || key=='a'){
-				System.out.println(key);
+		if(key=='A' || key=='a'||key=='d'||key=='D'||key=='W'||key=='w'||key=='s'||key=='S'){
+			//System.out.println(key);
+			drawAnimal(Spongebob ,key);
 		}
 				
 		
 	}
-
+	 int x=0;
+	 int y=0;
+	 int width=0;
+	 int height=0;
 	private void drawAnimal(Graphics g,Character key)
 	{
-		
+		if(x-width==0||x-width==getWidth()){
+		if(key=='N'){
+		Spongebob=g.create();
+		Spongebob.setColor(Color.yellow);
+		 x=getWidth();
+		 y=getHeight();
+		 width=getWidth()/5;
+		 height=getHeight()/5;
+		Spongebob.fillRect(x-width, y-height, width, height);
+		}else if(key=='A'||key=='a'){
+		Spongebob.setColor(new Color(155,155,255));
+		Spongebob.fillRect(x-width, y-height, width, height);
+		Spongebob.setColor(Color.yellow);
+		x=x-5;
+		Spongebob.fillRect(x-width, y-height, width, height);
+		}else if(key=='D'||key=='d'){
+			Spongebob.setColor(new Color(155,155,255));
+			Spongebob.fillRect(x-width, y-height, width, height);
+			Spongebob.setColor(Color.yellow);
+			x=x+5;
+			Spongebob.fillRect(x-width, y-height, width, height);
+		}else if(key=='w'||key=='W'){
+			Spongebob.setColor(new Color(155,155,255));
+			Spongebob.fillRect(x-width, y-height, width, height);
+			Spongebob.setColor(Color.yellow);
+			y=y-5;
+			Spongebob.fillRect(x-width, y-height, width, height);
+		}else if(key=='s'||key=='S'){
+			Spongebob.setColor(new Color(155,155,255));
+			Spongebob.fillRect(x-width, y-height, width, height);
+			Spongebob.setColor(Color.yellow);
+			y=y+5;
+			Spongebob.fillRect(x-width, y-height, width, height);
+		}
+		}
 	}
 	
 	private void drawHabitat(Graphics g)
