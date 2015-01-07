10cf59004ce112b7d1f30b2b42ac1a5a65aea890
rpoore <rpoore@neisd.net>
2015-01-07 11:29:16 -0600

    Initial Commit

---
+++ ClassZoo4/src/AnimalTemp.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.applet.*;
+
+public class AnimalTemp extends Applet{
+	
+	public void paint(Graphics g)
+	{
+		drawAnimal(g);
+	}
+
+	private void drawAnimal(Graphics g)
+	{
+		
+	}
+	
+	private void drawHabitat(Graphics g)
+	{
+		
+	}
+}
