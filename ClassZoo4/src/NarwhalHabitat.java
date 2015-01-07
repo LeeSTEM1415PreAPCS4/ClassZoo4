7070e0c41674e7d454212d98a822a1f1b09cb506
epena6656 <epena6656@001LAB102010533.neisd.ad>
2015-01-07 12:10:35 -0600

    Added the Narwhal

---
+++ ClassZoo4/src/NarwhalHabitat.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import java.applet.*;
+
+public class AnimalTemp extends Applet{
+	
+	public void paint(Graphics g)
+	{
+		drawAnimal(g);
+	}
+
+	private void drawAnimal(Graphics g)
+	{
+		
+	}
+	
+	private void drawHabitat(Graphics g)
+	{
+
+		g.drawRect(10,10,800,800);
+		g.drawOval(300,300,100,100);
+		g.setColor(Color.white);
+
+	}
+}
