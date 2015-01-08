0fe50ff5e908c9baf0b557a25cdbd102c39d135b
epena6656 <epena6656@001LAB102010533.neisd.ad>
2015-01-08 12:28:01 -0600

    Changes

--- ClassZoo4/src/NarwhalHabitat.java
+++ ClassZoo4/src/NarwhalHabitat.java
@@ -6,19 +6,24 @@ public class NarwhalHabitat extends Applet{
 	public void paint(Graphics g)
 	{
 		drawAnimal(g);
+		drawHabitat(g);
 	}
 
 	private void drawAnimal(Graphics g)
 	{
 		
+		g.setColor(new Color(109,141,145));
+		g.fillArc(350,200,175,300,180,-135);
+		//g.setColor(Color.black);
+		g.fillArc(410,229,105,105,45,-155);
+		g.drawPolygon()
 	}
 	
 	private void drawHabitat(Graphics g)
 	{
-
-		g.drawRect(10,10,800,800);
-		g.drawOval(300,300,100,100);
-		g.setColor(Color.white);
-
+		g.setColor(Color.black);
+		g.drawRect(10,10,800,700);
+		g.fillOval(350,300,100,100);
+		
 	}
 }
