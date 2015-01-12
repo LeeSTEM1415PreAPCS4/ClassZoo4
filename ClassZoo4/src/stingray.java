5d602988768bab361c0a565daf3fc944fe027103
vpatamalla5403 <vpatamalla5403@001LAB102010542.neisd.ad>
2015-01-12 11:49:09 -0600

    Added Stingray

---
+++ ClassZoo4/src/stingray.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.applet.*;
+
+public class stingray extends Applet {
+
+public void paint (Graphics g)
+{
+	g.setColor(Color.black);
+	g.drawRect(0, 0, 800, 600);
+g. setColor(Color.cyan);
+g.fillRect(0, 300, 800, 300);
+}
+}
