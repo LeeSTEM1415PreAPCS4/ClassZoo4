437a5e0f671354a89f84c651690f21a4ce9f1f55
mwalker2917 <mwalker2917@001LAB102010528.neisd.ad>
2015-02-09 11:45:25 -0600

    Jellyfish in JFrame

--- ClassZoo4/src/Jellyfish.java
+++ ClassZoo4/src/Jellyfish.java
@@ -1,135 +1,269 @@
-import java.awt.*;
-import java.applet.*;
+import java.awt.Color;
+//import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.TexturePaint;
+//import java.awt.Insets;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class Jellyfish extends Applet implements Runnable{
-	private boolean eyesopen;
-	private boolean randomColor;
-	Thread runner;
-	
-	public void paint(Graphics g)
-	{
-		drawHabitat(g);
-		drawAnimal(g);
-	}
-
-	private void drawHabitat(Graphics g)
-	{
-		{
-			Color water = new Color(70, 194, 217);
-
-			g.setColor(water);
-			g.fillRect(0, 0, 1000, 1000);
-
-			Random rndInt = new Random(6277);
-			
-
-			for (int z = 0; z<=25; z++)
-				{
-					int ex = rndInt.nextInt(900);
-					int why = rndInt.nextInt(1000);
-					g.setColor(Color.black);
-					g.drawOval(ex, why, 50, 50);
-					g.setColor(Color.white);
-					g.fillOval(ex, why, 50, 50);
-					
-					
-				}
-			
-			
-			
-			
-}
-		}
-	private void drawAnimal(Graphics g)
-	{
-		{
-		
-	    	 if (eyesopen)
-		    	  eyesopen = false;
-		      else
-		    	  eyesopen = true;
-
-			
-		//Jellyfish
-			g.setColor(Color.magenta);
-			g.fillOval(400, 200, 200, 200);
-			g.fillRect(400, 300, 200, 100);
-
-			int xx = 405;
-			int xxx = 595;
-			for (int x = 400; x<=595; x=x+20)
-			{
-
-				g.fillRect(x, 400, 2, 150);
-				g.fillRect(xx, 400, 2, 150);
-				g.fillRect(xxx, 400, 2, 150);
-				xx = xx + 15;
-				xxx = xxx - 15;
-				
-			}
-			
-			if (eyesopen==true)
-			{
-			//Draw Eyes
-			g.setColor(Color.black);
-			g.fillOval(460, 275, 25, 25);
-			g.fillOval(510, 275, 25, 25);
-			
-			//"I'm watching you"
-			Random rdInt = new Random();
-			Color random = new Color(rdInt.nextInt(243), rdInt.nextInt(243), rdInt.nextInt(243));
-			g.setFont(new Font("Impact", Font.TRUETYPE_FONT, 60)); 
-			g.setColor(random);
-			g.drawString("I'm", 100, 75);
-			g.drawString("watching", 30, 175);
-			g.drawString("you", 100, 275);
-			}
-			else
-			{
-			//Blink Eyes
-			g.setColor(Color.magenta);
-			g.fillOval(460, 275, 25, 25);
-			g.fillOval(510, 275, 25, 25);
-			}
-		
-		
-		}
-		}
-		public void run() {
-		     while (true){
-		    if (eyesopen==true)
-		    {
-		     
-		    	repaint();
-		      try { Thread.sleep(100); }
-		       catch (InterruptedException e) { }
-		      
-		      
-		     }
-		     
-		     else{
-		       repaint();
-		      
-		      try { Thread.sleep(5000); }
-		       catch (InterruptedException e) { }
-		      
-		     }
-		     }
-		   }
-		
-		 public void start() {
-		     if (runner == null) {
-		       runner = new Thread(this);
-		       runner.start();
-		     }
-		   }
-	
-
-	
-}
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 
 
 
 
+//import java.util.Timer;
+//import java.util.TimerTask;
+//import java.awt.Event.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.awt.Toolkit;
+import java.io.File;
+import java.io.IOException;
+
+
+ public class Jellyfish extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+		public Jellyfish() {
+	        initUI();
+	    }
+
+	    public final void initUI() {
+
+	        DrawPanel panel = new DrawPanel();
+	        add(panel);
+	        
+	        Toolkit tk = Toolkit.getDefaultToolkit();
+	        int wai = ((int) tk.getScreenSize().getWidth());
+	        int ehx = ((int) tk.getScreenSize().getHeight());
+	        setSize(wai, ehx);
+	        setTitle("I AM WATCHING YOU");
+	        setLocationRelativeTo(null);
+	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    }
+
+	    public static void main(String[] args) {
+
+	        SwingUtilities.invokeLater(new Runnable() {
+	            @Override
+	            public void run() {
+	                Jellyfish ex = new Jellyfish();
+	                ex.setVisible(true);
+	            }
+	        });
+	    }
+	    
+	    class DrawPanel extends JPanel implements KeyListener
+	    
+	    {
+	   	 private  int jx = 400;
+	   	private  int jy = 200;
+	   	 
+	   	 /*public void moveIt(int x, int y)
+	   	 {
+	   		 jx += x;
+	   		 jy += y;
+	   	 }*/
+	   	/**
+	   	 * 
+	   	 */
+	   	
+	   	private static final long serialVersionUID = 1L;
+	   	private Graphics2D Jelly;
+	   	private boolean eyesopen;
+	   	//private Thread runner;
+	   //private	BufferedImage sandy;
+	   //	private TexturePaint sandytp;
+	   	 
+
+	   	public void init(){
+	       	
+	       	addKeyListener( this );
+	       	
+	   	}
+	   	
+	   	private void theDrawing(Graphics g)  {
+	   		Graphics2D g2d = (Graphics2D) g;
+	   		
+	   			//loadImages();
+	   			drawHabitat(g2d);
+	   			drawAnimal(g2d, 'N');
+	   		 
+	   	}
+	   	
+	   private void drawHabitat(Graphics2D g2d) {
+	   		Color water = new Color (39, 128, 216);
+	   		g2d.setColor(water);
+	   		g2d.fillRect(0, 0, 1600, 1000);
+	   		
+	   		//sandytp = new TexturePaint(sandy, new Rectangle(10, 10, 100, 200));
+	   		//g2d.setPaint(sandytp);
+	   		//g2d.fillRect(0, 700, 1600, 200);
+
+	   		Random rndInt = new Random(62770);
+	   		
+
+	   		for (int z = 0; z<=40; z++)
+	   			{
+	   				int ex = rndInt.nextInt(1600);
+	   				int why = rndInt.nextInt(700);
+	   				g2d.setColor(Color.black);
+	   				g2d.drawOval(ex, why, 50, 50);
+	   				g2d.setColor(Color.white);
+	   				g2d.fillOval(ex, why, 50, 50);
+	   			}
+	   		
+	    }
+	   		
+	   	
+
+	   public void keyPressed(KeyEvent arg0) {
+	   	Character key=arg0.getKeyChar();
+	   	if(key=='A' || key=='a'||key=='d'||key=='D'||key=='W'||key=='w'||key=='s'||key=='S'){
+	       	drawAnimal(Jelly, key);
+	   	}
+	   }
+	       	
+	   	/*private void loadImages() {
+	   		try{
+	   			sandy = ImageIO.read(this.getClass().getResource("sandy.jpg"));
+	   		} catch (IOException ex) {
+	   			System.out.println("Uh, error!");
+	   			Logger.getLogger(DrawPanel.class.getName()).log(Level.SEVERE, null, ex);
+	   		}
+	   	}*/
+	   	 
+
+	   		
+	   		
+	   	
+	   	private void drawAnimal(Graphics2D g2d, Character key){
+	   		{
+	   			{
+	   			
+	   		    	 if (eyesopen)
+	   			    	  eyesopen = false;
+	   			      else
+	   			    	  eyesopen = true;
+
+	   				
+	   			//Jellyfish
+	   				
+	   				g2d.setColor(Color.magenta);
+	   				
+	   				g2d.fillOval(jx, jy, 200, 200);
+	   				g2d.fillRect(jx, jy+100, 200, 100);
+
+	   				int xx = jx+5;
+	   				int xxx = jx+195;
+	   				for (int x = jx; x<=jx+195; x=x+20)
+	   				{
+
+	   					g2d.fillRect(x, jy+200, 2, 150);
+	   					g2d.fillRect(xx, jy+200, 2, 150);
+	   					g2d.fillRect(xxx, jy+200, 2, 150);
+	   					xx = xx + 15;
+	   					xxx = xxx - 15;
+	   					
+	   				}
+	   				
+	   				if (eyesopen==true)
+	   				{
+	   				//Draw Eyes
+	   				g2d.setColor(Color.black);
+	   				g2d.fillOval(jx+60, jy+75, 25, 25);
+	   				g2d.fillOval(jx+110, jy+75, 25, 25);
+	   				
+	   				/*//"I'm watching you"
+	   				Random rdInt = new Random();
+	   				Color random = new Color(rdInt.nextInt(243), rdInt.nextInt(243), rdInt.nextInt(243));
+	   				g2d.setFont(new Font("Impact", Font.TRUETYPE_FONT, 60)); 
+	   				g2d.setColor(random);
+	   				g2d.drawString("Use 'a', 'w', 's', 'd'", 1000, 700);
+	   				g2d.drawString("to pop all the bubbles", 980, 775);
+	   				*/
+	   				}
+	   				else
+	   				{
+	   				//Blink Eyes
+	   					g2d.setColor(Color.magenta);
+	   					g2d.fillOval(460, 275, 25, 25);
+	   					g2d.fillOval(510, 275, 25, 25);
+	   				}
+	   			
+	   			
+
+	   			}
+	   		}
+	   			
+	   	
+	   }
+
+	   	
+	   		
+	   		
+	   		
+	   	    
+	   	
+	   	
+	    @Override
+	    public void paintComponent(Graphics g) {
+	   	//moveIt(1, 1);
+	   	 super.paintComponent(g);
+	   	 theDrawing(g);
+	   	 
+	   	 
+	   	 if (eyesopen==true)
+	   	    {
+	   	     
+	   	    	repaint();
+	   	    	
+	   	      try { Thread.sleep(100); }
+	   	       catch (InterruptedException e) { }
+	   	      
+	   	      
+	   	     }
+	   	     
+	   	     else{
+	   	    	 
+	   	       repaint();
+	   	      
+	   	      try { Thread.sleep(5000); }
+	   	       catch (InterruptedException e) { }
+	   	      
+	   	     }
+	   	     
+	    	}
+
+
+	   @Override
+	   public void keyReleased(KeyEvent e) {
+	   	// TODO Auto-generated method stub
+	   	
+	   }
+
+	   @Override
+	   public void keyTyped(KeyEvent e) {
+	   	// TODO Auto-generated method stub
+	   	
+	   }
+	    }
+	    
+
+	    
+	    
+ }
+ 
+ 
+ 
+ 
+ 
